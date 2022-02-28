485f84032002e5eaba0f7fc2f8180d8f3b70e5c4
Oleg Stenyakin <oleg.stenyakin@yandex.ru>
2022-02-28 18:39:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Simple Python parsing script to plot a histogram of the presence of any genre in the TOP 250 series of https://www.kinopoisk.ru/.
+
+It will be very convenient to run and look at the result of this script in Jupyter Notebook:
+```console
+jupyter notebook
+```
---
+++ kinopars.py
@@ -0,0 +1,57 @@
+
+
+import requests
+from bs4 import BeautifulSoup as BS
+import re
+from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+
+#
+# Select series info 
+#
+t = ''
+for p in range(1, 6) :
+    r = requests.get("https://www.kinopoisk.ru/lists/movies/series-top250/?page=" + str(p))
+    #print(r.text)
+    sleep(5)
+    t += r.text 
+soup = BS( t, features="html.parser")
+all_s = soup.find_all('div', class_='styles_root__3a8vf')
+
+#
+# Select genre of the series 
+#
+genre = []
+for film in all_s :
+    g = film.find('span', class_='desktop-list-main-info_truncatedText__2Q88H').text
+
+    try :
+        found = re.search(' • (.+?)\xa0', g).group(0)
+    except AttributeError :
+        found = 'XXX' # error handling
+
+    f = found[3:-1]
+    genre.append( f )
+
+#
+# Count number of unique genre keys
+#
+from collections import Counter
+c = Counter(genre)
+#print(c, len(c))
+#print(c.keys())
+
+#
+# Create a hist to show series distribution by genre
+#
+f = plt.figure(figsize = (20, 10)) # Set figure size
+b = [x+n for n in range(0, len(c)) for x in [0.0, 1.0]]+[len(c)] # Set ticks positions on the center of each bin
+plt.hist(genre, rwidth=0.9, bins=b, align='left') # Create a hist
+plt.ylabel("Количество раз", size=22) # Set y label 
+plt.grid(axis='y')
+plt.show()
+
+#
+# END
+#
